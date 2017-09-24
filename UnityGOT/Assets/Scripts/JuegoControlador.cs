dc17925775ca39564dd29f18c2958bef0de8b023
Unknown <moncho.alfonso@gmail.com>
2017-09-24 02:11:52 -0300

    Escenas 1 y 2 listas

--- UnityGOT/Assets/Scripts/JuegoControlador.cs
+++ UnityGOT/Assets/Scripts/JuegoControlador.cs
@@ -1,8 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class JuegoControlador : MonoBehaviour {
+
+	[Range(0f,0.20f)]
+	public float parallaxSpeed = 0.04f;
+	public RawImage fondo;
+	public RawImage plataforma;
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +17,9 @@ public class JuegoControlador : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		float finalSpeed = parallaxSpeed * Time.deltaTime;
+		fondo.uvRect = new Rect(fondo.uvRect.x + finalSpeed, 0f, 1f, 1f);
+		plataforma.uvRect = new Rect(plataforma.uvRect.x + finalSpeed * 4, 0f, 1f, 1f);
 		
 	}
 }
---
+++ UnityGOT/Assets/Scripts/JuegoControlador2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JuegoControlador2 : MonoBehaviour {
+
+	[Range(0f,0.20f)]
+	public float parallaxSpeed = 0.06f;
+	public RawImage fondo2;
+	public RawImage plataforma2;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float finalSpeed = parallaxSpeed * Time.deltaTime;
+		fondo2.uvRect = new Rect(fondo2.uvRect.x + finalSpeed, 0f, 1f, 1f);
+		plataforma2.uvRect = new Rect(plataforma2.uvRect.x + finalSpeed * 4, 0f, 1f, 1f);
+
+	}
+}
