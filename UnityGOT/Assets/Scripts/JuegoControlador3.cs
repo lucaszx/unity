0837288602bb5c2f92506b4299ce6f71201d71fe
juanqpablo <30931737+juanqpablo@users.noreply.github.com>
2017-09-26 17:08:57 -0300

    Add files via upload

---
+++ UnityGOT/Assets/Scripts/JuegoControlador3.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;                  //Este script controla todas las funcionalidades generales del juego
+using UnityEngine.UI;           //importamos UnityEngine.UI para utilizar componentes de interfaz
+
+
+public class JuegoControlador3 : MonoBehaviour {
+
+    public float parallaxSpeed = 0.02f;
+    public RawImage fondo3;                             //definimos variables publicas para gestionar 
+    public RawImage plataforma3;                       //tanto la velocidad del fondo como la plataforma y ademas para
+	// Use this for initialization                    // importar estos componentes y utilizarlos en este Script
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float finalSpeed = parallaxSpeed * Time.deltaTime;
+        fondo3.uvRect = new Rect(fondo3.uvRect.x + finalSpeed,0f,1f,1f);
+        plataforma3.uvRect = new Rect(plataforma3.uvRect.x + finalSpeed*4, 0f, 1f, 1f);
+    }
+}
